7eada2692a5a4e8997ceca863374a331f2547987
Rahul Swar <rahulswar@ip-192-168-0-199.ap-south-1.compute.internal>
2023-03-02 13:17:26 +0530

    added junit unit tests

--- src/test/java/com/turtlemint/MotorInsuranceCRUDApplication/MotorInsuranceCrudApplicationTests.java
+++ src/test/java/com/turtlemint/MotorInsuranceCRUDApplication/MotorInsuranceCrudApplicationTests.java
@@ -3,6 +3,7 @@ package com.turtlemint.MotorInsuranceCRUDApplication;
 import com.turtlemint.MotorInsuranceCRUDApplication.model.Profile;
 import com.turtlemint.MotorInsuranceCRUDApplication.repository.ProfileRepository;
 import com.turtlemint.MotorInsuranceCRUDApplication.service.ProfileService;
+
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,7 +15,7 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -26,6 +27,7 @@ class MotorInsuranceCrudApplicationTests {
 	@MockBean
 	private ProfileRepository profileRepository;
 
+	// JUnit Unit Tests
 	@Test
 	public void getAllProfilesTest() {
 		Profile profileTest1 = new Profile(12345, "FW", "suzuki", "swift");
@@ -36,16 +38,30 @@ class MotorInsuranceCrudApplicationTests {
 
 	@Test
 	public void getProfileTest(){
-		long requestId = 394339;
-		Profile profile = new Profile(394339, "FW", "suzuki", "baleno");
-		when(profileRepository.findById(requestId)).thenReturn(profile);
-		assertEquals(profile, profileService.getProfile(requestId));
+		Profile profile = new Profile(12345, "FW", "suzuki", "baleno");
+		when(profileRepository.findById(profile.getRequestId())).thenReturn(profile);
+		assertEquals(profile, profileService.getProfile(profile.getRequestId()));
 	}
 
 	@Test
 	public void createProfileTest(){
 		Profile profile = new Profile(12345, "FW", "kia", "seltos");
 		when(profileRepository.save(profile)).thenReturn(profile);
-		assertEquals(profileService.createProfile(profile), profile.getRequestId());
+		assertEquals(profileService.createProfile(profile), profile);
+	}
+
+	@Test
+	public void updateProfileTest(){
+		Profile profileTest1 = new Profile(12345, "FW", "suzuki", "swift");
+		Profile profileTest2 = new Profile(12345, "FW", "suzuki", "ertiga");
+		when(profileRepository.save(profileTest2)).thenReturn(profileTest2);
+		assertEquals(profileService.updateProfile(profileTest1.getRequestId(), profileTest2), profileTest2);
+	}
+
+	@Test
+	public void deleteProfileTest(){
+		Profile profile = new Profile(12345, "FW", "kia", "seltos");
+		profileService.deleteProfile(profile.getRequestId());
+		verify(profileRepository, times(1)).deleteById(profile.getRequestId());
 	}
 }
